56ae2c9050cb44f3711b36fbdea97e7a34fcea6f
macarron001 <rpinoon16@gmail.com>
2022-06-13 20:28:23 +0800

    reworked  service model test

--- spec/models/service_spec.rb
+++ spec/models/service_spec.rb
@@ -1,21 +1,26 @@
 require 'rails_helper'
 
-RSpec.describe Profile, type: :model do
+RSpec.describe Service, type: :model do
 
   before(:each) do
-    @admin = admin.create(
+    @admin = User.create!(
       email: 'new@admin.com',
       password: 'password',
       role: 'admin'
     )
+    
+    @service_params = {
+      name: 'Tooth Extraction',
+      price: 500,
+      branch: 'all'
+    }
   end
 
   subject {
     described_class.new(
       name: 'Tooth Extraction',
-      description: 'pulling a tooth',
       price: 500,
-      branch: 'all',
+      branch: 'all'
     )
   }
 
@@ -43,34 +48,25 @@ RSpec.describe Profile, type: :model do
     end
   end
 
-  context 'associations' do
-    it { should belong_to(:appointment) }
-    it { should belong_to(:transaction) }
-    it { should belong_to(:patient_record) }
-  end
-
   context 'admin capabilities' do
     it 'should be able to create a service' do
-      @admin.create_service(
-        'Multiple Tooth Extraction', 
-        'pooling a set of teeth',
-        1500,
-        'all'
-       )
+      @admin.create_service(@service_params)
 
        expect(Service.all.count).to eq(1)
     end
 
     it 'should be able to edit a service' do
-      @subject.update!(price: 600)
+      service = @admin.create_service(@service_params)
+      service.update!(price: 600)
 
-      expect(@subject.price).to eq(600)
+      expect(service.price).to eq(600)
     end
 
     it 'should be able to delete a service' do
-      @subject.destroy
-
-      expect(@subject).not_to exist
+      service = @admin.create_service(@service_params)
+      service.delete
+      
+      expect(Service.first).to eq(nil)
     end
   end
 end
