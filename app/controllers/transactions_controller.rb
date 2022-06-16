68eec3128a3aac5ed2c61b74156d18416d888242
macarron001 <rpinoon16@gmail.com>
2022-06-17 01:18:22 +0800

    updated transactions controller

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -1,15 +1,39 @@
 class TransactionsController < ApplicationController
   before_action :authenticate_user!
-  before_action :get_patient_record
+  before_action :get_patient_record, except: [:index, :show_transaction]
+  before_action :get_transaction, only: [:show_transaction]
+
+  def index
+    transactions = Transaction.all
+    render json: transactions
+  end
 
   def create_transaction
-    Transaction.create_transaction(transaction_params, @patient_record)
+    transaction = current_user.create_transaction(transaction_params, @patient_record)
 
+    if transaction_params[:status] == 'full'
     render json: json = {
       status: 201, 
-      message: `Transaction created!`,
-      transaction: transaction,
+      message: 'Payment Success!',
+      transaction: transaction[0],
     }, status: :ok
+    else
+      render json: json = {
+        status: 201, 
+        message: 'Payment Success!',
+        transaction: transaction[0],
+        profile: transaction[1]
+      }, status: :ok
+    end
+  end
+
+  def patient_transactions
+    transactions = @patient_record.transactions
+    render json: transactions
+  end
+
+  def show_transaction
+    render json: @transaction
   end
 
   private
@@ -17,7 +41,11 @@ class TransactionsController < ApplicationController
     params.require(:transaction).permit(:services, :status, :branch, :amount, :remaining, :payment_type)
   end
 
+  def get_transaction
+    @transaction = Transaction.find(params[:id])
+  end
+
   def get_patient_record
-    @patient_record = PatientRecord.find(params[:idd])
+    @patient_record = PatientRecord.find(params[:id])
   end
 end
