d1dac0c4264e5c8e756543251a09f2451e9cdf6e
macarron001 <rpinoon16@gmail.com>
2022-06-16 19:06:17 +0800

    removed dentist authentication requirement

--- app/controllers/patient_records_controller.rb
+++ app/controllers/patient_records_controller.rb
@@ -1,6 +1,5 @@
 class PatientRecordsController < ApplicationController
   before_action :authenticate_user!
-  before_action :authenticate_dentist!
   before_action :get_user
   before_action :get_patient_record, only: [:update_record]
 
@@ -36,12 +35,5 @@ class PatientRecordsController < ApplicationController
 
   def get_user
     @user = current_user
-  end
-
-  def authenticate_dentist!
-    render json: json = {
-      status: 401, 
-      message: 'Unauthorized user.',
-    }, status: :unauthorized unless current_user.role == 'dentist'
   end
 end
