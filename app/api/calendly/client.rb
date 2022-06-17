85536aed9c1453f5bef8f0982b9c91b2b1e545a3
alfredosdpiii <alfredo.sdpiii@gmail.com>
2022-06-17 16:34:41 +0800

    ✨ Added features  for calendly api wrapper

--- app/api/calendly/client.rb
+++ app/api/calendly/client.rb
@@ -1,11 +1,37 @@
 module Calendly
   class Client
+    ORGANIZATION = 'https://api.calendly.com/organizations/2a7498fb-bf14-40b4-971a-16dd78de8285'
+
     def self.show_me
       response = Request.call('get', '/users/me')
     end
 
     def self.show_events
-      response = Request.call('get', '/scheduled_events')
+      response = Request.call('get', "/scheduled_events?organization=#{ORGANIZATION}")
     end
+
+    def self.show_specific_event(id)
+      response = Request.call('get', "/scheduled_events?organization=#{ORGANIZATION}&id=#{id}")
+    end
+
+    def self.show_specific_guest_invitees(event_id)
+      response = Request.call('get', "/scheduled_events/#{event_id}/invitees/")
+    end
+
+    def self.show_specific_guest(event_id, guest_id)
+      response = Request.call('get', "/scheduled_events/#{event_id}/invitees/#{guest_id}")
+    end
+
+    def self.cancel_specific_event(event_id)
+      response = Request.call('get', "/scheduled_events/#{event_id}/cancellation")
+    end
+
+    def self.create_single_use_scheduling_link
+      response = Request.call('post', '/scheduling_links')
+    end
+
+    # def self.show_events
+    #   response = Request.call('get', "/scheduled_events?organization=#{ORGANIZATION}")
+    # end
   end
 end
--- app/api/calendly/request.rb
+++ app/api/calendly/request.rb
@@ -3,13 +3,11 @@ module Calendly
     TOKEN = 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwczovL2F1dGguY2FsZW5kbHkuY29tIiwiaWF0IjoxNjU1MjE1NzY4LCJqdGkiOiI5ZDczYTU4MS1lOWZhLTQ1MDUtODdkMC1lNzBjZWQ5ODNjMWUiLCJ1c2VyX3V1aWQiOiJjNTJlOTBjZS02MTkyLTQ3MDYtOGIxOS04NTBlZDNlNjMxZTQifQ.nZO6Y47Vl1i3BNpP6jMWecitkaxeFYkMdvj5ym1Xhrk'
     BASE_URL = 'http://api.calendly.com'
     UUID = 'c52e90ce-6192-4706-8b19-850ed3e631e4'
-    ORGANIZATION = 'https://api.calendly.com/organizations/2a7498fb-bf14-40b4-971a-16dd78de8285'
-     'https://api.calendly.com/organizations/2a7498fb-bf14-40b4-971a-16dd78de8285'
 
     def self.call(http_method, endpoint)
       result = RestClient::Request.execute(
         method: http_method,
-        url: "#{BASE_URL}#{endpoint}?organization=#{ORGANIZATION}",
+        url: "#{BASE_URL}#{endpoint}",
         headers: { 'Authorization' => TOKEN.to_s,
                    'Content-Type' => 'application/json' }
       )
