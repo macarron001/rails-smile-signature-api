fd642f3a98e200fdc743241ed60cf43bcf47276c
macarron001 <rpinoon16@gmail.com>
2022-07-02 03:07:09 +0800

    revised service model

--- app/models/service.rb
+++ app/models/service.rb
@@ -1,13 +1,9 @@
 class Service < ApplicationRecord
   validates :name, presence: true
-  validates :price, presence: true, numericality: {greater_than: 0}
-  validates :branch, presence: true, :inclusion => { :in => ['marcos-alvarez', 'almanza', 'all']}
-
+  
   def self.create_service(service_params)
     Service.create!(
-      :name => service_params[:name],
-      :price => service_params[:price],
-      :branch => service_params[:branch]
+      :name => service_params[:name]
     )
   end
 end
